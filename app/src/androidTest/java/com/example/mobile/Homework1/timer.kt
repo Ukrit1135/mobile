eb6409534f7eaffb58a02fd2690f7aecc90f577f
Ukrit1135 <1631051641135@rmutr.ac.th>
2022-09-02 11:09:18 +0700

    edit Ex3

--- app/src/androidTest/java/com/example/mobile/Homework1/timer.kt
+++ app/src/androidTest/java/com/example/mobile/Homework1/timer.kt
@@ -2,7 +2,7 @@ package com.example.mobile.Homework1
 
 fun main() {
     print("Enter millisecond : ")
-    val timemilli = readLine()!!.toLong()
+    var timemilli = readLine()!!.toLong()
     CaltimerFunction(timemilli)
 }
 
@@ -16,40 +16,40 @@ fun CaltimerFunction (timemilli:Long) {
     var month1 : Long = 0
     var year1 : Long = 0
 
-    if(timemilli1>=1000){
-         milli1= timemilli1%1000
+    if(timemilli1>=1000) {
+        milli1 = timemilli1 % 1000
         timemilli1 /= 1000
 
-        if(timemilli1>=60){
-            second1 = timemilli1%60
+        if (timemilli1 >= 60) {
+            second1 = timemilli1 % 60
             timemilli1 /= 60
 
-            if(timemilli1>=60){
-                minute1 = timemilli1%60
+            if (timemilli1 >= 60) {
+                minute1 = timemilli1 % 60
                 timemilli1 /= 60
 
-                if(timemilli1>=24){
-                    hour1 = timemilli1%24
+                if (timemilli1 >= 24) {
+                    hour1 = timemilli1 % 24
                     timemilli1 /= 24
 
-                    if(timemilli1>=30){
-                        day1 = timemilli1%30
+                    if (timemilli1 >= 30) {
+                        day1 = timemilli1 % 30
                         timemilli1 /= 30
 
-                        if(timemilli1>=12){
-                            month1 = timemilli1%12
+                        if (timemilli1 >= 12) {
+                            month1 = timemilli1 % 12
                             timemilli1 /= 12
 
-                            if(timemilli1>=1){
-                                year1 = timemilli1%1
+                            if (timemilli1 >= 1) {
+                                year1 = timemilli1 % 1
                             }
-                        }
-                    }
-                }
-            }
-        }
-    }else
-        milli1 = timemilli1
+                        }else{month1= timemilli1 % 30}
+                    }else{day1= timemilli1 % 24}
+                }else{hour1= timemilli1 % 60}
+            }else{minute1= timemilli1 % 60}
+        }else{second1= timemilli1 % 1000}
+    }else{milli1 += timemilli1}
+
 println("""-----------------------------
 Year        : $year1
 Month       : $month1
